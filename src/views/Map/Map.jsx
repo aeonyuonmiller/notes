954bcf29f4d9e5a8e6b44aa864de051829e8bf5a
aeonyuonmiller <aym1@mail.com>
2021-11-29 19:30:15 +0100

    styled the view

--- src/views/Map/Map.jsx
+++ src/views/Map/Map.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import './Map.css'
+
+import Button from '../../components/Button/Button'
 
 const Map = () => {
     return (
-        <div className="wrapper">
-          <h1>Welcome to the Map</h1>  
-        </div>
+        <>
+            <div className="topnav">
+                <Button text="Leave note" />
+                <h3>Explore</h3>
+                <Button text="Profile"/>
+            </div>
+            <div className="map">
+
+            </div>
+        </>
     )
 }
 
