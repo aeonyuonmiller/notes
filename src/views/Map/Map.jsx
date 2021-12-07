fac5f064e861a75322c2be3bf96ddb3fb178fb1d
aeonyuonmiller <aym1@mail.com>
2021-12-07 15:32:26 +0100

    mapbox integration

--- src/views/Map/Map.jsx
+++ src/views/Map/Map.jsx
@@ -1,9 +1,16 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef, useEffect, useState } from 'react'
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import { Redirect } from 'react-router'
 import { AuthContext } from '../../context/authContext'
 import './Map.css'
 
 import Button from '../../components/Button/Button'
+
+const Karte = ReactMapboxGl({
+  accessToken:
+    'pk.eyJ1IjoiYWVvbnl1b25taWxsZXIiLCJhIjoiY2phMTIybmNsMjFjeTMzbGdpcGdiM3J6ayJ9.FmtdgLWmMf4vgsagMsk-JQ'
+});
 
 const Map = () => {
     const { user } = useContext(AuthContext);
@@ -14,9 +21,15 @@ const Map = () => {
             <div className="topnav">
                 <Button text="Leave note" />
                 <h3>Explore</h3>
-                <Button text="Profile"/>
+                <Button text="Logout"/>
             </div>
-            <div className="map">
+            <div id="map">
+                <Karte style="mapbox://styles/mapbox/streets-v9"
+                containerStyle={{ height: '100%', width: '100%', borderRadius: '20px' }}>
+                    <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+                        <Feature coordinates={[31.00, 51.3233379650232]} zoom={20} />
+                    </Layer>
+                </Karte>;
             </div>
         </>
     )
