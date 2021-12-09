7cd9c7bafd0fa03b5b25150ec4a59b52c04ed90d
aeonyuonmiller <aym1@mail.com>
2021-12-09 14:47:19 +0100

    updated Menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -6,7 +6,8 @@ const Menu = ({ closeModal }) => {
     return (
         <div className="backdrop">
             <div className="wrapperMenu">
-                <div className="close" onClick={closeModal}>&times;</div>
+                <div className="close" onClick={closeModal} tabIndex="1">&times;</div>
+                <div className="logout" onClick={closeModal}>Logout</div>
                 <p>Keep looking for notes...</p>
             </div>
         </div>
