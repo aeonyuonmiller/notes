0717d4e0ef221e746adb9c7717c5ddcf9fa4afa4
aeonyuonmiller <aym1@mail.com>
2021-12-18 20:47:32 +0100

    working on exit-transition

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -7,10 +7,10 @@ const Menu = ({ closeModal, logoutBtn }) => {
 
     return (
         <AnimatePresence exitBeforeEnter>
-        <motion.div className="backdrop" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-            <motion.div className="wrapperMenu" initial={{ opacity: 0, x: 300, rotate: 5 }} animate={{ opacity: 1, x: 0, rotate: 0 }} exit={{ opacity: 0, x: -100 }} transition={{ type: "spring", stiffness: 700, damping: 70 }}>
+        <motion.div className="backdrop" key="backdropp" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+            <motion.div className="wrapperMenu" initial={{ scale: 0.95, opacity: 0, x: 300, rotate: 8 }} animate={{ scale: 1, opacity: 1, x: 0, rotate: 0 }} exit={{ opacity: 0, x: -100 }} transition={{ type: "spring", stiffness: 700, damping: 70 }}>
                 <div className="close" onClick={closeModal} tabIndex="1">&times;</div>
-                <div className="logout" onClick={logoutBtn} tabIndex="2">Logout</div>
+                <motion.div className="logout" whileHover={{ scale: 1.1 }} initial={{ scale: 0 }} animate={{ scale: 1 }} onClick={logoutBtn} tabIndex="2">Logout</motion.div>
                 <div className="no-notes">
                     <IlluNote />
                         <motion.p initial={{ opacity: 0, y: 10 }} animate={{ opacity: 1, y: 0 }} transition={{ type: "spring", stiffness: 700, damping: 70 }}>Keep looking for notes...</motion.p>
